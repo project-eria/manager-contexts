577b43366e640b57827c04626be3c97be4c30625
Ferllings <contact@ferllings.me>
2023-12-23 18:04:46 +0100

    [feat] eria-core v1.7.0

--- go.mod
+++ go.mod
@@ -3,29 +3,36 @@ module manager-context
 go 1.19
 
 require (
-	github.com/project-eria/eria-core v1.1.0
-	github.com/project-eria/go-wot v1.1.0
-	github.com/rs/zerolog v1.28.0
+	github.com/project-eria/eria-core v1.7.0
+	github.com/project-eria/go-wot v1.5.0
+	github.com/rs/zerolog v1.29.0
+	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df
 )
 
 require (
-	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/fasthttp/websocket v1.5.0 // indirect
-	github.com/gofiber/fiber/v2 v2.38.1 // indirect
-	github.com/gofiber/websocket/v2 v2.1.0 // indirect
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/fasthttp/websocket v1.5.2 // indirect
+	github.com/go-co-op/gocron v1.36.0 // indirect
+	github.com/gofiber/fiber/v2 v2.43.0 // indirect
+	github.com/gofiber/websocket/v2 v2.1.5 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/gookit/goutil v0.6.14 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/klauspost/compress v1.15.11 // indirect
+	github.com/klauspost/compress v1.16.3 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
-	github.com/savsgio/gotils v0.0.0-20220530130905-52f3993e8d6d // indirect
-	github.com/stretchr/testify v1.8.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/philhofer/fwd v1.1.2 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
+	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
+	github.com/tinylib/msgp v1.1.8 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.40.0 // indirect
+	github.com/valyala/fasthttp v1.45.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.1.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
-
-// replace github.com/project-eria/eria-core => ../eria-core
-
-// replace github.com/project-eria/go-wot => ../go-wot
--- main.go
+++ main.go
@@ -1,37 +1,35 @@
 package main
 
 import (
+	"github.com/project-eria/go-wot/thing"
+
 	eria "github.com/project-eria/eria-core"
 	"github.com/project-eria/go-wot/dataSchema"
 	"github.com/project-eria/go-wot/interaction"
+	"github.com/project-eria/go-wot/producer"
 	zlog "github.com/rs/zerolog/log"
+	"golang.org/x/exp/slices"
 )
 
 var config = struct {
-	Host        string             `yaml:"host"`
-	Port        uint               `yaml:"port" default:"80"`
-	ExposedAddr string             `yaml:"exposedAddr"`
-	Contexts    map[string]Context `yaml:"contexts"`
+	Contexts []string `yaml:"contexts" required:"true"`
 }{}
-
-type Context struct {
-	Title string      `yaml:"title" required:"true"`
-	Desc  string      `yaml:"desc" required:"true"`
-	Type  string      `yaml:"type" required:"true"`
-	Value interface{} `yaml:"value" required:"true"`
-}
 
 func main() {
 	defer func() {
 		zlog.Info().Msg("[main] Stopped")
 	}()
-
-	eria.Init("ERIA Shutter Manager")
+	eria.Init("ERIA Contexts Manager")
 	// Loading config
-	configManager := eria.LoadConfig(&config)
+	eria.LoadConfig(&config)
+	td := setThing()
+	eriaThing, _ := eria.Producer("").AddThing("", td)
 
-	eriaServer := eria.NewServer(config.Host, config.Port, config.ExposedAddr, "")
+	setHandlers(eriaThing)
+	eria.Start("")
+}
 
+func setThing() *thing.Thing {
 	td, _ := eria.NewThingDescription(
 		"eria:manager:context",
 		eria.AppVersion,
@@ -40,35 +38,101 @@ func main() {
 		[]string{},
 	)
 
-	for ref, context := range config.Contexts {
-		// TODO different data types
-		booleanData := dataSchema.NewBoolean(false)
-		property := interaction.NewProperty(
-			ref,
-			context.Title,
-			context.Desc,
-			false,
-			false,
-			true,
-			booleanData,
-		)
-		td.AddProperty(property)
-	}
+	activesData := dataSchema.NewArray([]string{}, 0, 0)
+	activesProperty := interaction.NewProperty(
+		"actives",
+		"Actives Contexts",
+		"List of contexts that are currently actives",
+		true,
+		false,
+		true,
+		nil,
+		activesData,
+	)
+	td.AddProperty(activesProperty)
 
-	eriaThing, _ := eriaServer.AddThing("", td)
+	isActiveData := dataSchema.NewBoolean(false)
+	isActiveProperty := interaction.NewProperty(
+		"isActive",
+		"Is context active",
+		"Tell if a specific context is active",
+		true,
+		false,
+		true,
+		map[string]dataSchema.Data{
+			"context": dataSchema.NewString("", 0, 0, ""),
+		},
+		isActiveData,
+	)
+	td.AddProperty(isActiveProperty)
 
-	for ref := range config.Contexts {
-		ref := ref // Copy https://go.dev/doc/faq#closures_and_goroutines
-		eriaThing.AddChangeCallBack(ref, func(value interface{}) {
-			if context, in := config.Contexts[ref]; in {
-				zlog.Trace().Str("property", ref).Interface("value", value).Msg("[main:changeCallBack] Change detected, saving config file")
+	setContextInputData := dataSchema.NewString("", 0, 0, "")
+	setContextOutputData := dataSchema.NewBoolean(false)
+	setContext := interaction.NewAction(
+		"setContext",
+		"Set context",
+		"Set the context as active",
+		&setContextInputData,
+		&setContextOutputData,
+	)
+	td.AddAction(setContext)
 
-				// Note: cannot directly assign to struct field config.Contexts[ref].Value in map
-				context.Value = value
-				config.Contexts[ref] = context
-				configManager.Save()
-			}
-		})
-	}
-	eriaServer.StartServer()
+	unsetContextInputData := dataSchema.NewString("", 0, 0, "")
+	unsetContextOutputData := dataSchema.NewBoolean(false)
+	unsetContext := interaction.NewAction(
+		"unsetContext",
+		"Unset context",
+		"Set the context as inactive",
+		&unsetContextInputData,
+		&unsetContextOutputData,
+	)
+	td.AddAction(unsetContext)
+	return td
+}
+
+func setHandlers(eriaThing producer.ExposedThing) {
+	eriaThing.SetPropertyReadHandler("actives", func(t producer.ExposedThing, name string, parameters map[string]interface{}) (interface{}, error) {
+		zlog.Trace().Str("property", "actives").Interface("value", config.Contexts).Msg("[main:propertyReadHandler] Value get")
+		return config.Contexts, nil
+	})
+
+	eriaThing.SetPropertyReadHandler("isActive", func(t producer.ExposedThing, name string, parameters map[string]interface{}) (interface{}, error) {
+		// The presence and the value type of the `context` options has been checked by Protocol Binding
+		return slices.Contains(config.Contexts, parameters["context"].(string)), nil
+	})
+
+	eriaThing.SetPropertyObserveHandler("isActive", func(t producer.ExposedThing, name string, parameters map[string]interface{}) (interface{}, error) {
+		// The presence and the value type of the `context` options has been checked by Protocol Binding
+		return slices.Contains(config.Contexts, parameters["context"].(string)), nil
+	})
+
+	eriaThing.SetObserverSelectorHandler("isActive", func(emitOptions map[string]interface{}, listenerOptions map[string]interface{}) bool {
+		return emitOptions["context"] == listenerOptions["context"]
+	})
+
+	eriaThing.SetActionHandler("setContext", func(value interface{}, parameters map[string]interface{}) (interface{}, error) {
+		context := value.(string)
+		i := slices.Index(config.Contexts, context)
+		if i == -1 {
+			// Not in array yet
+			config.Contexts = append(config.Contexts, context)
+			eriaThing.EmitPropertyChange("actives", config.Contexts, map[string]interface{}{"context": context})
+			eriaThing.EmitPropertyChange("isActive", true, map[string]interface{}{"context": context})
+			return true, nil
+		}
+		return false, nil // Nothing was done
+	})
+
+	eriaThing.SetActionHandler("unsetContext", func(value interface{}, parameters map[string]interface{}) (interface{}, error) {
+		context := value.(string)
+		i := slices.Index(config.Contexts, context)
+		if i != -1 {
+			// In array
+			config.Contexts = slices.Delete(config.Contexts, i, i+1)
+			eriaThing.EmitPropertyChange("actives", config.Contexts, map[string]interface{}{"context": context})
+			eriaThing.EmitPropertyChange("isActive", false, map[string]interface{}{"context": context})
+			return true, nil
+		}
+		return false, nil // Nothing was done
+	})
 }
